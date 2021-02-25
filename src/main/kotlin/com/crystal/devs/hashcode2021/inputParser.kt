4256c1431929ded04ea6b53b0cc2c7873fa90ada
Ryunos <elise.rubio@4sh.fr>
2021-02-25 19:34:45 +0100

    feat: Parse input
    Co-authored-by: Tom Pietri <to.pietri@gmail.com>

--- src/main/kotlin/com/crystal/devs/hashcode2021/inputParser.kt
+++ src/main/kotlin/com/crystal/devs/hashcode2021/inputParser.kt
@@ -1,7 +1,30 @@
 package com.crystal.devs.hashcode2021
 
 fun parseInput(input: List<String>): ParsedInput {
-    return ParsedInput(input = input)
+    val (duration, _, streetsNumber, carsNumber, scorePerCar) = input[0].split(" ").map { it.toInt() }
+
+    val streets = input.subList(1, streetsNumber + 1).map {
+        val (from, to, name, length) = it.split(" ")
+        Street(name, length.toInt(), from.toInt(), to.toInt())
+    }.map { it.name to it }.toMap()
+
+    val cars = input.subList(streetsNumber + 1, input.size).map {
+        val split = it.split(" ")
+        val carStreets = split.subList(1, split.size)
+        Car(carStreets.toMutableList(), streets.get(carStreets[0])!!)
+    }
+
+    return ParsedInput(simulationDurationInSeconds = duration,
+            scorePerCar = scorePerCar,
+            streets = streets,
+            cars = cars)
 }
 
-data class ParsedInput(val input : List<String>);
+data class ParsedInput(val simulationDurationInSeconds: Int,
+                       val scorePerCar: Int,
+                       val streets: Map<String, Street>,
+                       val cars: List<Car>)
+
+data class Street(val name: String, val length: Int, var from: Int, var to: Int)
+
+data class Car(val path: MutableList<String>, val currentStreet: Street)
