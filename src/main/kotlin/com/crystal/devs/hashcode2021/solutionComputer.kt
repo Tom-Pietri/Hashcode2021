cd733495c5c482c93eed11e4751334af53887fd4
Ryunos <elise.rubio@4sh.fr>
2021-02-25 20:01:01 +0100

    feat: Write Output
    Co-authored-by: Tom Pietri <to.pietri@gmail.com>

--- src/main/kotlin/com/crystal/devs/hashcode2021/solutionComputer.kt
+++ src/main/kotlin/com/crystal/devs/hashcode2021/solutionComputer.kt
@@ -1,9 +1,30 @@
 package com.crystal.devs.hashcode2021
 
+import java.io.PrintWriter
+
 fun computeSolution(parsedInput: ParsedInput): Solution {
-    return Solution(parsedInput)
+    return Solution(emptyList())
 }
 
-data class Solution(val solution: ParsedInput) {
+data class Solution(val intersectionsSchedules: List<IntersectionSchedule>) {
     fun computeScore() = 1;
+
+    fun print(out: PrintWriter) {
+        out.println(intersectionsSchedules.size)
+        intersectionsSchedules.forEach { it.print(out) }
+    }
+}
+
+data class IntersectionSchedule(val intersectionNumber: Int, val trafficLightSchedules: List<TrafficLightSchedule>) {
+    fun print(out: PrintWriter) {
+        out.println(intersectionNumber)
+        out.println(trafficLightSchedules.size)
+        trafficLightSchedules.forEach { it.print(out) }
+    }
+}
+
+data class TrafficLightSchedule(val duration: Int, val streetName: String) {
+    fun print(out: PrintWriter) {
+        out.println("$streetName $duration")
+    }
 }
--- src/main/kotlin/com/crystal/devs/hashcode2021/solutionWriter.kt
+++ src/main/kotlin/com/crystal/devs/hashcode2021/solutionWriter.kt
@@ -4,6 +4,6 @@ import java.io.File
 
 fun writeSolution(solution: Solution, inputName: String) {
     File("solution_$inputName.txt").printWriter().use { out ->
-        out.println("test")
+        solution.print(out)
     }
 }
